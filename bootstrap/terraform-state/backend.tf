e60516aed457abffdd0e91f55ce06d5ea29a8767
Shakir Ahmed Shah <shakirshahahmed@gmail.com>
2026-08-27 22:33:04 +0530

    Modified the storage main.tf file

---
+++ bootstrap/terraform-state/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-ealz-tfstate-eus2-001"
+    storage_account_name = "stealztfstate001"
+    container_name       = "tfstate"
+    key                  = "bootstrap.tfstate"
+    use_azuread_auth = true
+  }
+}
