aaea586f352b8c02fb8320454782aecdbd91599d
Shakir Ahmed Shah <shakirshahahmed@gmail.com>
2026-08-28 23:09:43 +0530

    updated modules for nsg rules

--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -158,31 +158,6 @@ variable "nsg_name" {
   type        = string
 }
 
-variable "nsg_security_rules" {
-  description = "Security rules for the Network Security Group"
-
-  type = list(object({
-    name                                       = string
-    description                                = optional(string)
-    protocol                                   = string
-    source_port_range                          = optional(string)
-    source_port_ranges                         = optional(list(string))
-    destination_port_range                     = optional(string)
-    destination_port_ranges                    = optional(list(string))
-    source_address_prefix                      = optional(string)
-    source_address_prefixes                    = optional(list(string))
-    destination_address_prefix                 = optional(string)
-    destination_address_prefixes               = optional(list(string))
-    access                                     = string
-    priority                                   = number
-    direction                                  = string
-    source_application_security_group_ids      = optional(list(string))
-    destination_application_security_group_ids = optional(list(string))
-  }))
-
-  default = []
-}
-
 # RT-vars:
 
 variable "route_table_name" {
