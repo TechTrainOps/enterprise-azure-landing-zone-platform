ce71c8c0da1f4826ecf605b6d0780ebdad20f015
Shakir Ahmed Shah <shakirshahahmed@gmail.com>
2026-08-29 17:13:59 +0530

    updated modules for PE

--- modules/networking/private-endpoint/output.tf
+++ modules/networking/private-endpoint/output.tf
@@ -12,8 +12,3 @@ output "network_interface_id" {
   description = "Network Interface ID associated with the Private Endpoint"
   value       = azurerm_private_endpoint.private_endpoint.network_interface[0].id
 }
-
-output "private_service_connection_state" {
-  description = "Private Service Connection status"
-  value       = azurerm_private_endpoint.private_endpoint.private_service_connection[0].private_connection_status
-}
