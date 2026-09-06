1a0c87b3d8d1211fa9350cbea8a5f31d4f69afe8
Shakir Ahmed Shah <shakirshahahmed@gmail.com>
2026-09-07 00:55:41 +0530

    updated folder for rg

---
+++ environments/dev/resource-groups/variables.tf
@@ -0,0 +1,14 @@
+variable "resource_group_name" {
+  description = "Name of the Azure resource group."
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region where the resource group is created."
+  type        = string
+}
+
+variable "tags" {
+  description = "Tags applied to the resource group."
+  type        = map(string)
+}
