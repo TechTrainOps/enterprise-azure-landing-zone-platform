4c5b293bb1d0a3f255e94060469be3ba78155280
Shakir Ahmed Shah <shakirshahahmed@gmail.com>
2026-09-09 01:51:20 +0530

    updated folder for monitoring

--- scripts/migration.ps1
+++ scripts/migration.ps1
@@ -1,4 +1,4 @@
-cd "$env:BUILD_SOURCESDIRECTORY\environments\dev"
+cd "$env:BUILD_SOURCESDIRECTORY\environments\dev\monitoring"
 
 Write-Host "Current Terraform directory:"
 Get-Location
@@ -9,12 +9,12 @@ if ($LASTEXITCODE -ne 0) {
     throw "Terraform init failed."
 }
 
-terraform state show 'module.key_vault_diagnostic_policy_monitoring_role.azurerm_role_assignment.role_assignment'
+terraform import 'module.linux_vm_data_collection_rule_association.azurerm_monitor_data_collection_rule_association.association' '/subscriptions/2d1634f0-7a97-4b55-b237-f5b12e52c45c/resourceGroups/rg-ealz-dev-eastus2/providers/Microsoft.Compute/virtualMachines/vm-ealz-dev-eastus2-001/providers/Microsoft.Insights/dataCollectionRuleAssociations/dcr-assoc-linux-vm'
 
-terraform state show 'module.key_vault_diagnostic_policy_log_analytics_role.azurerm_role_assignment.role_assignment'
+terraform import 'module.windows_vm_data_collection_rule_association.azurerm_monitor_data_collection_rule_association.association' '/subscriptions/2d1634f0-7a97-4b55-b237-f5b12e52c45c/resourceGroups/rg-ealz-dev-eastus2/providers/Microsoft.Compute/virtualMachines/vm-windows-ealz-dev-eastus2-001/providers/Microsoft.Insights/dataCollectionRuleAssociations/dcr-assoc-windows-vm'
 
-terraform state show 'module.storage_account_diagnostic_policy_monitoring_role.azurerm_role_assignment.role_assignment'
+terraform import 'module.vmss_data_collection_rule_association.azurerm_monitor_data_collection_rule_association.association' '/subscriptions/2d1634f0-7a97-4b55-b237-f5b12e52c45c/resourceGroups/rg-ealz-dev-eastus2/providers/Microsoft.Compute/virtualMachineScaleSets/vmss-ealz-dev-eastus2-001/providers/Microsoft.Insights/dataCollectionRuleAssociations/dcr-assoc-vmss'
 
-terraform state show 'module.storage_account_diagnostic_policy_log_analytics_role.azurerm_role_assignment.role_assignment'
+terraform state list | Select-String "data_collection_rule_association"
 
-terraform state show 'module.acr_diagnostic_policy_log_analytics_role_assignment.azurerm_role_assignment.role_assignment'
+terraform plan
