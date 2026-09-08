66b6f176f063aa645b609494a2f731910587d3a1
Shakir Ahmed Shah <shakirshahahmed@gmail.com>
2026-09-09 02:03:20 +0530

    updated folder for monitoring

--- scripts/migration.ps1
+++ scripts/migration.ps1
@@ -9,4 +9,6 @@ if ($LASTEXITCODE -ne 0) {
     throw "Terraform init failed."
 }
 
-terraform import 'module.compute_data_collection_rule.azurerm_monitor_data_collection_rule.compute' '/subscriptions/2d1634f0-7a97-4b55-b237-f5b12e52c45c/resourceGroups/rg-ealz-dev-eastus2/providers/Microsoft.Insights/dataCollectionRules/dcr-ealz-dev-eastus2-compute-001'
+terraform state list | Select-String "data_collection_rule"
+
+terraform plan
