3890f35f37c940f4da9cbfecf4411e181c784808
Shakir Ahmed Shah <shakirshahahmed@gmail.com>
2026-09-09 02:50:44 +0530

    updated folder for monitoring

--- scripts/migration.ps1
+++ scripts/migration.ps1
@@ -9,20 +9,21 @@ if ($LASTEXITCODE -ne 0) {
     throw "Terraform init failed."
 }
 
-terraform import 'module.subscription_activity_log_diagnostic_setting.azurerm_monitor_diagnostic_setting.diagnostic_setting' '/subscriptions/2d1634f0-7a97-4b55-b237-f5b12e52c45c|diag-subscription-activity-log'
+$acrRoleId = az role assignment list --assignee-object-id "6e7a1b8b-3bc7-4b8e-9574-36531760ef15" --all --query "[?roleDefinitionName=='Log Analytics Contributor' && scope=='/subscriptions/2d1634f0-7a97-4b55-b237-f5b12e52c45c/resourceGroups/rg-ealz-dev-eastus2/providers/Microsoft.OperationalInsights/workspaces/law-ealz-dev-eastus2-001'].id | [0]" -o tsv
+terraform import 'module.acr_diagnostic_policy_log_analytics_role_assignment.azurerm_role_assignment.role_assignment' $acrRoleId
 
-terraform import 'module.key_vault_diagnostic_setting.azurerm_monitor_diagnostic_setting.diagnostic_setting' '/subscriptions/2d1634f0-7a97-4b55-b237-f5b12e52c45c/resourceGroups/rg-ealz-dev-eastus2/providers/Microsoft.KeyVault/vaults/kv-ealz-dev-eastus2-001|diag-keyvault'
+$kvLogRoleId = az role assignment list --assignee-object-id "0407a8ec-bced-456f-a408-5b4dddba0b4e" --all --query "[?roleDefinitionName=='Log Analytics Contributor' && scope=='/subscriptions/2d1634f0-7a97-4b55-b237-f5b12e52c45c/resourceGroups/rg-ealz-dev-eastus2/providers/Microsoft.OperationalInsights/workspaces/law-ealz-dev-eastus2-001'].id | [0]" -o tsv
+terraform import 'module.key_vault_diagnostic_policy_log_analytics_role.azurerm_role_assignment.role_assignment' $kvLogRoleId
 
-terraform import 'module.storage_account_diagnostic_setting.azurerm_monitor_diagnostic_setting.diagnostic_setting' '/subscriptions/2d1634f0-7a97-4b55-b237-f5b12e52c45c/resourceGroups/rg-ealz-dev-eastus2/providers/Microsoft.Storage/storageAccounts/stealzdeveastus2001/blobServices/default|diag-storage-blob'
+$kvMonitoringRoleId = az role assignment list --assignee-object-id "0407a8ec-bced-456f-a408-5b4dddba0b4e" --all --query "[?roleDefinitionName=='Monitoring Contributor' && scope=='/subscriptions/2d1634f0-7a97-4b55-b237-f5b12e52c45c/resourceGroups/rg-ealz-dev-eastus2'].id | [0]" -o tsv
+terraform import 'module.key_vault_diagnostic_policy_monitoring_role.azurerm_role_assignment.role_assignment' $kvMonitoringRoleId
 
-terraform import 'module.acr_diagnostic_setting.azurerm_monitor_diagnostic_setting.diagnostic_setting' '/subscriptions/2d1634f0-7a97-4b55-b237-f5b12e52c45c/resourceGroups/rg-ealz-dev-eastus2/providers/Microsoft.ContainerRegistry/registries/crealzdeveastus2001|diag-acr'
+$storageLogRoleId = az role assignment list --assignee-object-id "3ed4f22a-077f-4bc7-9c13-06ac9b825152" --all --query "[?roleDefinitionName=='Log Analytics Contributor' && scope=='/subscriptions/2d1634f0-7a97-4b55-b237-f5b12e52c45c/resourceGroups/rg-ealz-dev-eastus2/providers/Microsoft.OperationalInsights/workspaces/law-ealz-dev-eastus2-001'].id | [0]" -o tsv
+terraform import 'module.storage_account_diagnostic_policy_log_analytics_role.azurerm_role_assignment.role_assignment' $storageLogRoleId
 
-terraform import 'module.linux_vm_memory_alert.azurerm_monitor_scheduled_query_rules_alert_v2.scheduled_query_alert' '/subscriptions/2d1634f0-7a97-4b55-b237-f5b12e52c45c/resourceGroups/rg-ealz-dev-eastus2/providers/Microsoft.Insights/scheduledQueryRules/alert-ealz-dev-linux-memory'
+$storageMonitoringRoleId = az role assignment list --assignee-object-id "3ed4f22a-077f-4bc7-9c13-06ac9b825152" --all --query "[?roleDefinitionName=='Monitoring Contributor' && scope=='/subscriptions/2d1634f0-7a97-4b55-b237-f5b12e52c45c/resourceGroups/rg-ealz-dev-eastus2'].id | [0]" -o tsv
+terraform import 'module.storage_account_diagnostic_policy_monitoring_role.azurerm_role_assignment.role_assignment' $storageMonitoringRoleId
 
-terraform import 'module.windows_vm_memory_alert.azurerm_monitor_scheduled_query_rules_alert_v2.scheduled_query_alert' '/subscriptions/2d1634f0-7a97-4b55-b237-f5b12e52c45c/resourceGroups/rg-ealz-dev-eastus2/providers/Microsoft.Insights/scheduledQueryRules/alert-ealz-dev-windows-memory'
-
-terraform import 'module.linux_vm_disk_alert.azurerm_monitor_scheduled_query_rules_alert_v2.scheduled_query_alert' '/subscriptions/2d1634f0-7a97-4b55-b237-f5b12e52c45c/resourceGroups/rg-ealz-dev-eastus2/providers/Microsoft.Insights/scheduledQueryRules/alert-ealz-dev-linux-disk'
-
-terraform import 'module.windows_vm_disk_alert.azurerm_monitor_scheduled_query_rules_alert_v2.scheduled_query_alert' '/subscriptions/2d1634f0-7a97-4b55-b237-f5b12e52c45c/resourceGroups/rg-ealz-dev-eastus2/providers/Microsoft.Insights/scheduledQueryRules/alert-ealz-dev-windows-disk'
+terraform state list | Select-String "role_assignment"
 
 terraform plan
