17c0a8adfd780d19bd89c9d8bf7c3c9761f3d414
Shakir Ahmed Shah <shakirshahahmed@gmail.com>
2026-09-09 01:58:36 +0530

    updated folder for monitoring

--- scripts/migration.ps1
+++ scripts/migration.ps1
@@ -9,12 +9,4 @@ if ($LASTEXITCODE -ne 0) {
     throw "Terraform init failed."
 }
 
-terraform import 'module.linux_vm_data_collection_rule_association.azurerm_monitor_data_collection_rule_association.association' '/subscriptions/2d1634f0-7a97-4b55-b237-f5b12e52c45c/resourceGroups/rg-ealz-dev-eastus2/providers/Microsoft.Compute/virtualMachines/vm-ealz-dev-eastus2-001/providers/Microsoft.Insights/dataCollectionRuleAssociations/dcr-assoc-linux-vm'
-
-terraform import 'module.windows_vm_data_collection_rule_association.azurerm_monitor_data_collection_rule_association.association' '/subscriptions/2d1634f0-7a97-4b55-b237-f5b12e52c45c/resourceGroups/rg-ealz-dev-eastus2/providers/Microsoft.Compute/virtualMachines/vm-windows-ealz-dev-eastus2-001/providers/Microsoft.Insights/dataCollectionRuleAssociations/dcr-assoc-windows-vm'
-
-terraform import 'module.vmss_data_collection_rule_association.azurerm_monitor_data_collection_rule_association.association' '/subscriptions/2d1634f0-7a97-4b55-b237-f5b12e52c45c/resourceGroups/rg-ealz-dev-eastus2/providers/Microsoft.Compute/virtualMachineScaleSets/vmss-ealz-dev-eastus2-001/providers/Microsoft.Insights/dataCollectionRuleAssociations/dcr-assoc-vmss'
-
-terraform state list | Select-String "data_collection_rule_association"
-
-terraform plan
+terraform import 'module.compute_data_collection_rule.azurerm_monitor_data_collection_rule.compute' '/subscriptions/2d1634f0-7a97-4b55-b237-f5b12e52c45c/resourceGroups/rg-ealz-dev-eastus2/providers/Microsoft.Insights/dataCollectionRules/dcr-ealz-dev-eastus2-compute-001'
