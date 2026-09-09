5516909038d2412d9057d6220491ed128f2746e8
Shakir Ahmed Shah <shakirshahahmed@gmail.com>
2026-09-09 19:11:48 +0530

    updated folder for compute

--- modules/compute/linux-virtual-machine/main.tf
+++ modules/compute/linux-virtual-machine/main.tf
@@ -26,7 +26,7 @@ resource "azurerm_linux_virtual_machine" "linux_vm" {
 
   availability_set_id = var.availability_set_id
 
-  disable_password_authentication = true
+  disable_password_authentication                        = true
   bypass_platform_safety_checks_on_user_schedule_enabled = true
 
   admin_ssh_key {
