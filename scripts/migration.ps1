a8332c97f67b78e04c9a6836bc3832a4fff28c6d
Shakir Ahmed Shah <shakirshahahmed@gmail.com>
2026-09-09 18:18:12 +0530

    updated folder for compute

--- scripts/migration.ps1
+++ scripts/migration.ps1
@@ -9,6 +9,4 @@ if ($LASTEXITCODE -ne 0) {
     throw "Terraform init failed."
 }
 
-terraform import 'module.managed_disk.azurerm_virtual_machine_data_disk_attachment.linux_vm_data_disk' '/subscriptions/2d1634f0-7a97-4b55-b237-f5b12e52c45c/resourceGroups/rg-ealz-dev-eastus2/providers/Microsoft.Compute/virtualMachines/vm-ealz-dev-eastus2-001/dataDisks/disk-ealz-dev-eastus2-001'
-
-terraform state list
+terraform state list 'module.availability_set.azurerm_availability_set.compute' | Select-String "resource_group_name|id"
